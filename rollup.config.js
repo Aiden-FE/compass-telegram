0c0d212d6fa9a15616fedd878dba4777791be166
Aiden <31586885+Aiden-FE@users.noreply.github.com>
2023-05-05 22:56:57 +0800

    Dev/axios (#11)
    * wip: 迁移axios
    
    * chore: upgrade deps
    
    * chore: update version
    
    * fix: build error
    
    ---------
    
    Co-authored-by: Aiden-FE <Aiden_FE@outlook.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,40 @@
-import json from '@rollup/plugin-json'
-import ts from "rollup-plugin-ts";
-import {builtinModules} from "module";
-import terser from "@rollup/plugin-terser";
-import cleanup from "rollup-plugin-cleanup";
-import summary from "rollup-plugin-summary";
-import {nodeResolve} from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import serve from "rollup-plugin-serve";
-import pkg from './package.json'
+import json from '@rollup/plugin-json';
+import ts from 'rollup-plugin-ts';
+import { builtinModules } from 'module';
+import terser from '@rollup/plugin-terser';
+import cleanup from 'rollup-plugin-cleanup';
+import summary from 'rollup-plugin-summary';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import serve from 'rollup-plugin-serve';
+import pkg from './package.json';
 
-const isProd = !(process.env.npm_lifecycle_event === 'dev');
+const isProd = !process.env.ROLLUP_WATCH;
 
 /**
  * @description 获取构建插件
  * @param {('serve'|'nodeResolve'|'commonjs'|'compiler'|'terser'|'cleanup'|'summary')[]} disablePlugins 待禁用的插件
+ * @param {{[key: string]: object}} options
  * @return {(Plugin|false|{generateBundle: generateBundle, name: string})[]}
  */
-function getPlugins(disablePlugins = []) {
+function getPlugins(disablePlugins = [], options = {}) {
   return [
     json(),
     ts(),
-    !disablePlugins.includes('serve') && !isProd && serve({
+    !disablePlugins.includes('serve') &&
+    !isProd &&
+    serve(options['serve'] || {
       port: 3000,
-      contentBase: '.'
+      contentBase: '.',
     }),
-    !disablePlugins.includes('nodeResolve') && nodeResolve({ browser: true }),
-    !disablePlugins.includes('commonjs') && isProd && commonjs(),
-    !disablePlugins.includes('terser') && isProd && terser(),
-    !disablePlugins.includes('cleanup') && isProd && cleanup({ comments: 'none' }),
-    !disablePlugins.includes('summary') && isProd && summary({
+    // 如果目标是node环境,需要提供选项{ exportConditions: ["node"] }以支持构建
+    !disablePlugins.includes('nodeResolve') && isProd && nodeResolve(options['nodeResolve'] || undefined),
+    !disablePlugins.includes('commonjs') && isProd && commonjs(options['commonjs'] || undefined),
+    !disablePlugins.includes('terser') && isProd && terser(options['terser'] || undefined),
+    !disablePlugins.includes('cleanup') && isProd && cleanup(options['cleanup'] || { comments: 'none' }),
+    !disablePlugins.includes('summary') &&
+    isProd &&
+    summary(options['summary'] || {
       totalLow: 1024 * 8,
       totalHigh: 1024 * 20,
       showBrotliSize: true,
@@ -52,19 +58,19 @@ function getExternal(additionalExternal = []) {
  * @param options 文档: https://www.rollupjs.com/guide/big-list-of-options
  * @return {Record<string, unknown>}
  */
-function getOutput(
-  options = { format: 'es' }
-) {
+function getOutput(options = {}) {
   return {
     dir: 'dist',
+    format: 'es',
     chunkFileNames: 'bundle/chunk.[format].[hash].js',
     entryFileNames: '[name].[format].js',
     sourcemap: isProd,
     ...options,
-  }
+  };
 }
 
 export default [
+  // esm bundle
   {
     input: 'src/main.ts',
     output: getOutput({
@@ -76,27 +82,35 @@ export default [
       include: ['src/**', 'index.html'],
     },
   },
-  isProd && {
-    input: 'src/main.ts',
-    output: getOutput({
-      exports: 'auto',
-      format: 'cjs',
-      entryFileNames: pkg.commonjs.replace('dist/', ''),
-    }),
-    external: getExternal(),
-    plugins: getPlugins(),
-  },
+  // umd bundle
   isProd && {
     input: 'src/main.ts',
     output: getOutput({
       format: 'umd',
       file: pkg.umd,
-      name: 'Telegram',
+      name: 'Telegram', // Set your library name.
       dir: undefined,
       chunkFileNames: undefined,
       entryFileNames: undefined,
+      exports: 'auto',
     }),
     external: getExternal(),
-    plugins: getPlugins(),
+    plugins: getPlugins(undefined, {
+      nodeResolve: { browser: true }
+    }),
   },
-].filter(item => !!item);
+  // commonjs bundle
+  // {
+  //   input: 'src/main.ts',
+  //   output: getOutput({
+  //     format: 'cjs',
+  //     chunkFileNames: undefined,
+  //     entryFileNames: pkg.commonjs.replace('dist/', ''),
+  //     exports: 'auto',
+  //   }),
+  //   external: getExternal(),
+  //   plugins: getPlugins(undefined, {
+  //     nodeResolve: { browser: false, exportConditions: ['node'] },
+  //   }),
+  // },
+].filter((item) => !!item);
